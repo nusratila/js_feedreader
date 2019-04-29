8cbdfbeeeda03b76ecc317b092783bb1253de8f9
Nusrat Jahan Ila <nusrat.ila@gmail.com>
2019-04-28 22:39:36 -0700

    initial change

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,47 +26,131 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
+        
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('all URLs are defined and not empty',function(){
+            for (let feed of allFeeds){
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('All names are defined and not empty',function(){
+            for (let feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+        it('The menu element is hidden by default',function(){
+            let isHidden = document.body.classList.contains("menu-hidden")
+            
+            expect(isHidden).toBe(true);
+        });
+        
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('The menu changes visibility when it is clicked.',function(){
+            let iconList = document.querySelector(".icon-list");
+            iconList.click();
+            let isHidden = document.body.classList.contains("menu-hidden")
+            expect(isHidden).toBe(false);
+            iconList.click();
+            isHidden = document.body.classList.contains("menu-hidden")
+            expect(isHidden).toBe(true);
+        });
 
+
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done){
+            loadFeed(0,function(){
+                done();
+            });
+        });
 
+        it('Has at least a single .entry element within the .feed',function(){
+            let feedDiv = document.querySelector(".feed");
+            let hasEntry = feedDiv.querySelectorAll("a.entry-link");
+
+            expect(hasEntry.length).not.toBe(0);
+        });
+
+        
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe("New Feed Selection", function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        let firstElefFeed0 = "";
+        let  firstElefFeed1 = "";
+        beforeEach(function(done){
+            loadFeed(2,function(){
+                done();
+
+            });
+
+            // code to get first element
+
+
+
+        });
+
+        it('ensures when a new feed is loaded',function(){
+            let feedDiv = document.querySelector(".feed");
+            firstElefFeed0 = feedDiv.querySelector("a").href;
+            console.log("first");
+            console.log(firstElefFeed0);
+            //loadfeed1  
+            //loadFeed(1,done()); 
+            (function(done){
+                loadFeed(3,function(){
+                    done();
+    
+                }); 
+            });
+            feedDiv = document.querySelector(".feed");
+            firstElefFeed1 = feedDiv.querySelector("a").href;
+
+            // code for first element feed1 let feedDiv = document.querySelector(".feed");
+            console.log("second");
+            console.log(firstElefFeed1);
+            expect(firstElefFeed0).not.toBe(firstElefFeed1);
+        });
+
+        
+    });
 }());
